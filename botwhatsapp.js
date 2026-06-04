5ec667639d24ce56b44904874019d02348e5ed3a
وسام تكنلوجي || Wesam Technology <wesam.technology7@gmail.com>
2026-06-04 16:23:02 +0300

    تعديل في اسم المطعم

--- botwhatsapp.js
+++ botwhatsapp.js
@@ -30,7 +30,7 @@ const groq = new Groq({ apiKey: apiKey });
 // Destination restaurant JID and details
 const RESTAURANT_NUMBER = "967773267572@s.whatsapp.net";
 const RESTAURANT_RAW_PHONE = "+967773267572";
-const RESTAURANT_NAME = " Alpha One";
+const RESTAURANT_NAME = " Alpha One ";
 
 // ==========================================
 // 🍔 المنيو الرسمي القابل للتعديل والتغيير
